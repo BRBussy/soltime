ebcbe5c87719e348b1cc16c8d649c378041e7583
Bernard Bussy <brbitzbussy@gmail.com>
2021-05-08 11:05:05 +0200

    add em all

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,2 @@
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
-};
-
-entrypoint!(process_instruction);
-fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
-}
+// inside lib.rs, only the following line should be in here
+pub mod entrypoint;
